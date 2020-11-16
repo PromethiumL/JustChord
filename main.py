4954848e0af745823e6bb5c3aa36118cc58b41bd
PromethiumL <PromethiumL@outlook.com>
2020-11-16 21:45:14 +0800

    Add current dir to path when launch main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
+import sys
+
+curpath = os.path.abspath(os.path.dirname(__file__))
 from JustChord.gui.app import JustChordApp
 
 if __name__ == '__main__':
-	app = JustChordApp()
+    app = JustChordApp()
